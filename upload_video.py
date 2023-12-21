6d23130ed8af85196c6558e9fe6eea939d549938
Mayank Chaudhary <72939306+mayank-2105@users.noreply.github.com>
2023-12-22 00:02:20 +0530

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -51,9 +51,10 @@ CLIENT_SECRETS_FILE = 'client_secret.json'
 
 # This OAuth 2.0 access scope allows an application to upload files to the
 # authenticated user's YouTube channel, but doesn't allow other types of access.
-SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
-API_SERVICE_NAME = 'youtube'
-API_VERSION = 'v3'
+YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
 
 VALID_PRIVACY_STATUSES = ('public', 'private', 'unlisted')
 
