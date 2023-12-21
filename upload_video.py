bdd1a45c7b7b1d75bde37445bbc387ada74e0655
Mayank Chaudhary <72939306+mayank-2105@users.noreply.github.com>
2023-12-21 23:56:48 +0530

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -17,7 +17,7 @@ import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import flow_from_clientsecrets
 
 
 # Explicitly tell the underlying HTTP transport library not to retry, since
@@ -60,10 +60,20 @@ VALID_PRIVACY_STATUSES = ('public', 'private', 'unlisted')
 
 # Authorize the request and store authorization credentials.
 def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_local_server()
-    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    
+
+  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+      scope=YOUTUBE_UPLOAD_SCOPE,
+      message=MISSING_CLIENT_SECRETS_MESSAGE)
+  
+    storage = Storage("%s-oauth2.json" % sys.argv[0])
+    credentials = storage.get()
+  
+    if credentials is None or credentials.invalid:
+      credentials = run_flow(flow, storage, args)
+  
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+      http=credentials.authorize(httplib2.Http()))
 
 
 def initialize_upload(youtube, options):
