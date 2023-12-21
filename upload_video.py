764b3750ff6f00056e5cb9f7c50a09362eddaf63
Mayank Chaudhary <72939306+mayank-2105@users.noreply.github.com>
2023-12-22 00:03:20 +0530

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -55,6 +55,23 @@ YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the API Console
+https://console.developers.google.com/
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+
 
 VALID_PRIVACY_STATUSES = ('public', 'private', 'unlisted')
 
